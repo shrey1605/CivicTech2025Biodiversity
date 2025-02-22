0aa8684dde25cc581968c77dba5f073552d47497
Ashi Kamra <ak118@wellesley.edu>
2025-02-22 16:45:20 -0500

    adding to my branch

--- Client/app.py
+++ Client/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st.title("Explore Flora from Harvard University Herbarium")
+st.title("Welcome to the Harvard University Herbarium")
+st.subheader("Explore the global flora within the Herbarium's collection")
 clicked = st.button("On Click")
+
